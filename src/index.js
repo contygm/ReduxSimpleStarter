baf47aa1922651c103203add544a9450d6b2710a
Genevieve Conty <genevieve.conty@AV-MBP-7208.local>
2017-05-25 22:25:47 -0400

    sec1 lec14

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import SearchBar from './components/SearchBar';
 
-import App from './components/app';
-import reducers from './reducers';
+// TODO: env variable
+const API_KEY = ''
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const App = () => {
+  return (
+    <div>
+      <SearchBar/>
+    </div>
+  );
+}
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+ReactDOM.render(<App />, document.querySelector('.container'));
